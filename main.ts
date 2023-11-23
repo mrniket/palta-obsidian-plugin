b9cb990e2050d15f0bf33e346f5c8882f5c900a5
Niket <niket@sumrise.email>
2023-11-23 17:26:21 +0000

    Use css file for setting css variables

--- main.ts
+++ main.ts
@@ -6,7 +6,6 @@ import PaltaNoteDefaults from "palta-note-test";
 export default class PaltaPlugin extends Plugin {
 	async onload() {
 		this.registerCustomElementIfNotExists();
-		this.addCSSVariables()
 		this.registerMarkdownCodeBlockProcessor(
 			"palta",
 			(source, el: HTMLElement, ctx) => {
@@ -16,12 +15,6 @@ export default class PaltaPlugin extends Plugin {
 		);
 	}
 
-	private addCSSVariables() {
-		const body = document.body;
-		const computedStyle = getComputedStyle(body);
-		body.style.setProperty("--palta-accent-color", computedStyle.getPropertyValue("--interactive-accent"));
-		body.style.setProperty("--palta-text-color", computedStyle.getPropertyValue("--text-normal"));
-	}
 
 	registerCustomElementIfNotExists() {
 		if (customElements.get("palta-note") !== undefined) {
